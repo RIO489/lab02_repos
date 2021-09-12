36f6c950c829f38b3eba6031c2b67bb9a0de665e
RIO489 <oleksander.hamaiunov.it.2021@lpnu.ua>
2021-09-12 15:32:10 +0300

    lab 2.1

---
+++ lab 2.1/lab 2.1.cpp
@@ -0,0 +1,27 @@
+﻿// Lab_02.cpp
+// < Гамаюнов Олександр >
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 5.2
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	double a; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+
+	cout << "a =  "; cin >> a;
+
+	z1 = 1- 1./4 * ( sin(a+a) * sin(a+a) ) + cos(a+a);
+	z2 = (cos(a) * cos(a)) + (cos(a) * cos(a) * cos(a) * cos(a));
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
